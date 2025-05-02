0f6d4ec6b3f510d0d3642054b1cf1414086d95dc
desireemezzotero <desireemezzotero1@gmail.com>
2025-05-02 16:29:48 +0200

    add cors

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,31 +3,34 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Portfolio;
+use Illuminate\Http\Request;
+
 
 class PostController extends Controller
 {
-    public function index(){
-        $project = Portfolio::with('type','technologies')->get();
-       
+    public function index()
+    {
+        $project = Portfolio::with('type', 'technologies')->get();
+
         return response()->json(
             [
-                "success"=>'true',
+                "success" => 'true',
                 "data" => $project
             ]
-            );
-        }
+        );
+    }
 
-    public function show(Portfolio $project){
+    public function show(Portfolio $project)
+    {
 
-        $project->load('type','technologies');
-       
+        $project->load('type', 'technologies');
+
         return response()->json(
             [
-                "success"=>'true',
+                "success" => 'true',
                 "data" => $project
             ]
-            ); 
+        );
     }
 }
---
+++ config/cors.php
@@ -0,0 +1,20 @@
+<?php
+return [
+
+  'paths' => ['api/*'],
+
+  'allowed_methods' => ['*'],
+
+  'allowed_origins' => [env('APP_FRONTEND_URL', 'http://127.0.0.1:8000/')],
+
+  'allowed_origins_patterns' => [],
+
+  'allowed_headers' => ['*'],
+
+  'exposed_headers' => [],
+
+  'max_age' => 0,
+
+  'supports_credentials' => false,
+
+];
